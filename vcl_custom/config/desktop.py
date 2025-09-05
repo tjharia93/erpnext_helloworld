5e91f82f78a53c8e75e6afb7c6f7da7790b0dca4
Tanuj Haria <tanuj.haria@vimit.com>
2025-09-05 18:48:24 +0300

    feat: initial commit of vcl_custom app with Hello World doctype

---
+++ vcl_custom/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "VCL Custom",
+            "color": "#2B3990",
+            "icon": "octicon octicon-smiley",
+            "type": "module",
+            "label": _("VCL Custom")
+        }
+    ]
